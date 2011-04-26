041debc532bd7d0a422fe27284dbee0c52f9ff1e
David Precious <davidp@preshweb.co.uk>
2011-04-26 12:36:29 +0100

    Quick module to check open pull requests.
    A Bot::BasicBot::Pluggable module to check the number of pull requests open on a
    GitHub projet.

---
+++ bot-basicbot-modules/GitHubPullRequests.pm
@@ -0,0 +1,67 @@
+# A quick Bot::BasicBot::Pluggable module to fetch a count of open pull requests
+# for a GitHub project.
+#
+# David Precious <davidp@preshweb.co.uk>
+
+package Bot::BasicBot::Pluggable::Module::GitHubPullRequests;
+use strict;
+use base 'Bot::BasicBot::Pluggable::Module';
+use LWP::Simple;
+use JSON;
+
+sub help {
+    return <<HELPMSG;
+Monitors outstanding pull requests on a GitHub project.
+
+Allows use of a !pr command to fetch the current count of open pull requests,
+and can also periodically announce them on a timer.
+HELPMSG
+}
+
+
+sub said {
+    my ($self, $mess, $pri) = @_;
+    warn "Saw $mess->{body} at priority $pri";
+    
+    #return unless $pri == 2;
+
+    if ($mess->{body} =~ /!pr \s* (\S+)?/xi) {
+        my $check_projects = $1 || $self->get('monitor_projects');
+        warn "Projects to check: $check_projects";
+        for my $project (split /,/, $check_projects) {
+            warn "Checking PRs for $project";
+            my $prs = _get_pull_request_count($project);
+            $self->say(
+                channel => $mess->{channel},
+                body => "Open pull requests for $project : $prs",
+            );
+        }
+        return 1; # "swallow" this message
+    }
+    return; # This message didn't interest us
+}
+
+
+sub _get_pull_request_count {
+    my $project = shift;
+    my $url = "http://github.com/api/v2/json/pulls/" . $project;
+    my $json = LWP::Simple::get($url)
+        or return "Unknown - error fetching $url";
+    my $pulls = JSON::from_json($json)
+        or return "Unknown - error parsing API response";
+
+    my %pulls_by_author;
+    $pulls_by_author{$_}++
+        for map { $_->{issue_user}{login} } @{ $pulls->{pulls} };
+    my $msg = scalar @{ $pulls->{pulls} } . " pull requests open (";
+    $msg .= join(", ", 
+        map  { "$_:$pulls_by_author{$_}" }
+        sort { $pulls_by_author{$b} <=> $pulls_by_author{$a} }
+        keys  %pulls_by_author 
+    );
+    $msg .= ")";
+    return $msg;
+}
+
+1;
+
