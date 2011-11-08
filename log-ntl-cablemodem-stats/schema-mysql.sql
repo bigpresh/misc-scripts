d567a58e410d13c9e2097d94cc977e6451df010d
David Precious <davidp@preshweb.co.uk>
2011-11-08 19:15:35 +0000

    Fetch modem event log entries too.
    Fetch event log entries from modem, store them in the DB, then clear the modem's
    event log so that we don't keep seeing the same entries multiple times.
    
    This is a bit of a dirty approach; there's a potential race condition for events
    appearing between us requesting the event log page and clearing the event log -
    those events would be lost.
    
    I don't think it's a big deal, but it's not perfect.
    
    Includes a refactor to use LWP::UserAgent instead of LWP::Simple, as I need to
    make a POST request, which LWP::Simple annoyingly doesn't do.  As a benefit,
    though, the authentication details can just be set up with a call to
    $ua->credentials once, rather than passed in every URL, so that's a little bit
    of win to brighten my evening (wow, what an exciting evening so far, eh? :) )

--- log-ntl-cablemodem-stats/schema-mysql.sql
+++ log-ntl-cablemodem-stats/schema-mysql.sql
@@ -1,6 +1,15 @@
 CREATE TABLE stats (
-    id int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `timestamp` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    `key`   VARCHAR(200),
-    `value` VARCHAR(200)
+    id int(11)    NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `timestamp`   TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    `key`         VARCHAR(200),
+    `value`       VARCHAR(200)
+);
+
+CREATE TABLE eventlog (
+    id int(11)    NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `timestamp`   TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    `first_time`  VARCHAR(200),
+    `last_time`   VARCHAR(200),
+    `priority`    VARCHAR(200),
+    `description` VARCHAR(250)
 );
--- log-ntl-cablemodem-stats/schema-sqlite3.sql
+++ log-ntl-cablemodem-stats/schema-sqlite3.sql
@@ -4,3 +4,13 @@ CREATE TABLE stats (
     key VARCHAR,
     value VARCHAR
 );
+
+CREATE TABLE eventlog (
+    id           INTEGER PRIMARY KEY,
+    timestamp    DATETIME DEFAULT CURRENT_TIMESTAMP,
+    first_time   VARCHAR,
+    last_time    VARCHAR,
+    priority     VARCHAR,
+    description  VARCHAR
+);
+
